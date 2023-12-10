1a9d7825828a763ef48c82e7bca8ecf153916c3e
A-Havryniuk <alinahavryniuk@gmail.com>
2023-12-10 21:19:40 +0200

    feat: add saving of image

--- src/app/affine-transformation/affine-transformation.component.ts
+++ src/app/affine-transformation/affine-transformation.component.ts
@@ -19,6 +19,13 @@ export class AffineTransformationComponent {
   eventsSubject: Subject<void> = new Subject<void>();
 
   emitEventToChild() {
+    this.p1 = { x: this.p1.x, y: this.p1.y }
+    this.p2 = { x: this.p2.x, y: this.p2.y }
     this.eventsSubject.next();
+  }
+
+  saveSubject: Subject<void> = new Subject<void>();
+  save() {
+    this.saveSubject.next()
   }
 }
